245dd9f7a7092985f31bb885b64eb550db737136
Your Name git config --global user.email you@example.com <you@example.com>
2025-11-11 10:29:50 +0600

    Initial Commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module cse213.group7.papermillfinal {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens cse213.group7.papermillfinal to javafx.fxml;
+    exports cse213.group7.papermillfinal;
+}
